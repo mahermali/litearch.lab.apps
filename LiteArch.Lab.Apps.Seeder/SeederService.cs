7b58337102b7d2704e81b3fa2e241d1ebf2e1318
Maher ali <maher.techmasters@gmail.com>
2021-03-05 13:36:34 -0500

    add seeder request to the api

--- LiteArch.Lab.Apps.Seeder/SeederService.cs
+++ LiteArch.Lab.Apps.Seeder/SeederService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -11,16 +12,25 @@ namespace LiteArch.Lab.Apps.Seeder
         private readonly IConfiguration _configuration;
         private static int _lastId = 0;
         private Random _random = new Random(Environment.TickCount);
+        private HttpClient _client;
 
         public SeederService(IConfiguration configuration)
         {
             _configuration = configuration;
+            _client = new HttpClient()
+            {
+                BaseAddress = new Uri(_configuration["Api:BaseUrl"])
+            };
         }
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
                 Console.WriteLine($"Seeding: {++_lastId}");
+                await _client.PostAsJsonAsync("api/seed", new
+                {
+                    Key=Environment.MachineName
+                }, cancellationToken);
                 await Task.Delay(_random.Next(500, 3000), cancellationToken);
             }
         }
